c41217a6137d464e02662e405331364e2e1f30b3
Giovanni Benner <giovanni.guilioti@benner.com.br>
2023-01-10 15:33:02 -0300

    finalizado

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
---1
+-- Consulta 1
 SELECT CONVERT(VARCHAR(5), 
 			DATEADD(minute,
 				DATEDIFF(MINUTE, '09:00', ins.hr_inspecao)
@@ -14,14 +14,16 @@ WHERE hr_inspecao IN (
 	ORDER BY hr_inspecao
 	DESC);
 
---2
+-- Professor, não consegui realizar a consulta 2, provavelmente por ter abstraido o modelo de forma errada,
+-- o resultado mostra apenas os horarios de trabalho e não o total.
+-- Consulta 2
 SELECT hr_inspecao as last_time
 FROM producao.Inspecao as i
 INNER JOIN producao.Ficha as f ON (i.cd_ficha = f.cd_ficha)
 INNER JOIN producao.Inspetor as p ON (f.cd_matricula = p.cd_matricula)
 WHERE f.dt_inspecao BETWEEN '20221201' and '20221222' AND p.nm_inspetor = 'Trancoso Da Silva';
 
---3
+-- Consulta 3
 SELECT cd_avaliacao, COUNT(1) as recorrencia
 FROM producao.Inspecao as pi
 INNER JOIN producao.Ficha as pf ON (pi.cd_ficha = pf.cd_ficha)
@@ -31,7 +33,7 @@ WHERE pf.dt_inspecao BETWEEN CONVERT(datetime, '20221201')
 GROUP BY cd_avaliacao
 ORDER BY recorrencia DESC;
 
---4
+-- Consulta 4
 SELECT nm_inspetor, COUNT(1) as	produtos_tr
 FROM producao.Inspetor as pi
 INNER JOIN producao.Ficha as pfi ON (pi.cd_matricula = pfi.cd_matricula)
@@ -40,7 +42,7 @@ WHERE pin.cd_avaliacao = 'TR'
 GROUP BY nm_inspetor
 ORDER BY produtos_tr DESC;
 
---5
+-- Consulta 5
 SELECT CONCAT(cd_produto, '.', linha_produto, '.', aa_produto) as cod_produto
 FROM producao.Inspecao
 WHERE cd_avaliacao = 'OK' AND cd_produto IN (
